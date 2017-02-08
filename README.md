f8e66be38d7791949fe8813f73a41ae9dee1e85b
Brett Mierzejewski <bstetson@visione.com>
2017-02-07 20:03:49 -0500

    canvas demo to start

---
+++ README.md
@@ -0,0 +1,2 @@
+##Using canvas
+#####Will be working with canvas in my free time for the next few months trying to get a better understanding
---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let express = require('express'),
+    app = express(),
+    port = 4444;
+
+app.use(express.static('.'));
+
+app.listen(port, function() {
+    console.log('express train #', port);
+});
